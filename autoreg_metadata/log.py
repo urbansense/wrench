69ada39a56435e390822d2d983e297541d800153
Jeffrey Limnardy <jeffreylimnardy@yahoo.com>
2025-02-10 14:35:02 +0100

    add file loghandler

--- autoreg_metadata/log.py
+++ autoreg_metadata/log.py
@@ -9,6 +9,7 @@ def setup_logging(level=logging.DEBUG):
 
     # Enhanced formatter with file, line number, and function name
     handler = logging.StreamHandler()
+    fileHandler = logging.FileHandler("autoreg-metadata.log")
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
@@ -16,6 +17,7 @@ def setup_logging(level=logging.DEBUG):
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(level)
+    logger.addHandler(fileHandler)
 
     # Prevent propagation to avoid duplicate logs
     logger.propagate = False
