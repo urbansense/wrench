161460c05abe0015108cad551f7b5a2c3a369cfc
Jeffrey Limnardy <jeffreylimnardy@yahoo.com>
2025-04-09 15:10:09 +0200

    remove from init

--- wrench/components/__init__.py
+++ wrench/components/__init__.py
@@ -2,13 +2,10 @@ from .cataloger import Cataloger
 from .grouper import Grouper
 from .harvester import Harvester
 from .metadatabuilder import MetadataBuilder
-from .registry import ComponentRegistry, discover_components
 
 __all__ = [
     "Cataloger",
     "Grouper",
     "Harvester",
     "MetadataBuilder",
-    "ComponentRegistry",
-    "discover_components",
 ]
