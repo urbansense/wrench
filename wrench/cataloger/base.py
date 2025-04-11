149b12e5bcd0aa12c11ce137d694555b98fb1707
Jeffrey Limnardy <jeffreylimnardy@yahoo.com>
2025-04-11 12:30:57 +0200

    enable state for cataloger

--- wrench/cataloger/base.py
+++ wrench/cataloger/base.py
@@ -20,5 +20,22 @@ class BaseCataloger(ABC):
         self.logger = logger.getChild(self.__class__.__name__)
 
     @abstractmethod
-    def register(self, service: CommonMetadata, groups: Sequence[CommonMetadata]):
+    def register(
+        self,
+        service: CommonMetadata,
+        groups: Sequence[CommonMetadata],
+        managed_entries: list[str] | None,
+    ) -> list[str]:
+        """
+        Registers the service and its groups to the catalog.
+
+        Args:
+            service (CommonMetadata): The service to be registered.
+            groups (Sequence(CommonMetadata)): The groups which should be
+                registered under the service.
+            managed_entries: list(str): The entries created by the catalogger.
+
+        Returns:
+            list(str): The list of keys or URLs the resources are registered under.
+        """
         pass
