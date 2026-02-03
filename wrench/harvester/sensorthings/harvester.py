ee2cc08b7c94404c6453066aba228cf73804007c
Jeffrey Limnardy <jeffreylimnardy@yahoo.com>
2026-02-03 23:08:22 +0100

    implement multidatastreams

--- wrench/harvester/sensorthings/harvester.py
+++ wrench/harvester/sensorthings/harvester.py
@@ -6,7 +6,7 @@ from wrench.models import Device, TimeFrame
 
 from .client import SensorThingsClient
 from .config import PaginationConfig
-from .models import Thing
+from .models import Datastream, MultiDatastream, Thing
 
 
 class SensorThingsHarvester(BaseHarvester):
@@ -53,18 +53,10 @@ class SensorThingsHarvester(BaseHarvester):
         devices = []
 
         for thing in things:
-            time_frame = None
-            for ds in thing.datastreams:
-                if ds.phenomenon_time:
-                    start_time = datetime.fromisoformat(
-                        ds.phenomenon_time.split("/")[0]
-                    )
-                    latest_time = datetime.fromisoformat(
-                        ds.phenomenon_time.split("/")[1]
-                    )
-                    time_frame = TimeFrame(
-                        start_time=start_time, latest_time=latest_time
-                    )
+            time_frame = self._build_timeframes(
+                thing.datastreams, thing.multidatastreams
+            )
+            datastreams, sensors, observed_properties = self._extract_stream(thing)
 
             device = Device(
                 id=thing.id,
@@ -72,11 +64,9 @@ class SensorThingsHarvester(BaseHarvester):
                 description=thing.description,
                 locations=thing.location,
                 time_frame=time_frame,
-                datastreams={ds.name for ds in thing.datastreams},
-                sensors={ds.sensor.name for ds in thing.datastreams},
-                observed_properties={
-                    ds.observed_property.name for ds in thing.datastreams
-                },
+                datastreams=datastreams,
+                sensors=sensors,
+                observed_properties=observed_properties,
                 properties=thing.properties,
                 _raw_data=thing.model_dump(),
             )
@@ -84,3 +74,48 @@ class SensorThingsHarvester(BaseHarvester):
             devices.append(device)
 
         return devices
+
+    def _build_timeframes(
+        self, ds: list[Datastream], mds: list[MultiDatastream]
+    ) -> TimeFrame | None:
+        # use datastreams if available, mds if ds isn't available
+        streams = mds if not ds and mds else ds
+        return self._extract_outer_bounds(streams)
+
+    def _extract_outer_bounds(
+        self, streams: list[Datastream] | list[MultiDatastream]
+    ) -> TimeFrame | None:
+        start_times = []
+        end_times = []
+
+        for ds in streams:
+            if ds.phenomenon_time:
+                parts = ds.phenomenon_time.split("/")
+                start_times.append(datetime.fromisoformat(parts[0]))
+                end_times.append(datetime.fromisoformat(parts[1]))
+
+        if not start_times:
+            return None
+
+        return TimeFrame(start_time=min(start_times), latest_time=max(end_times))
+
+    def _extract_stream(self, thing: Thing) -> tuple[set[str], set[str], set[str]]:
+        if thing.datastreams:
+            return (
+                {ds.name for ds in thing.datastreams},
+                {ds.sensor.name for ds in thing.datastreams},
+                {ds.observed_property.name for ds in thing.datastreams},
+            )
+
+        if thing.multidatastreams:
+            return (
+                {mds.name for mds in thing.multidatastreams},
+                {mds.sensor.name for mds in thing.multidatastreams},
+                {
+                    op.name
+                    for mds in thing.multidatastreams
+                    for op in mds.observed_properties
+                },
+            )
+
+        return set(), set(), set()
